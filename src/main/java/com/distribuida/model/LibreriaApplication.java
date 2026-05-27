10c8240ce892e1637566983d82606a46e5ae464d
AC <achiriboga@tecnologicoismac.edu.ec>
2026-05-27 10:24:27 -0500

    Cambios

---
+++ src/main/java/com/distribuida/model/LibreriaApplication.java
@@ -0,0 +1,29 @@
+package com.distribuida.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaApplication.class, args);
+		System.out.println("HOLA MUNDO DESDE SPRING BOOT...!!");
+
+
+		Cliente cliente = new Cliente(
+				1
+				,"1726323234"
+				,"Alan"
+				,"Brito"
+				,"Quito"
+				,"0983456256"
+				,"abrito@correo.com"
+
+		);
+		System.out.println(cliente.toString());
+
+
+	}
+
+}
